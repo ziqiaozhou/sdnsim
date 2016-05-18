90ab58cc7de5c56a4122ae1d220edca72ce071ed
ziqiaozhou <ziqiao@cs.unc.edu>
2016-05-18 02:50:58 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #1->num test $2->flownum $3->rulenum $4->msizerate $5->ttlmax $6->interval $7runtime
 for i in `seq 1 $1`
 do
-nohup ./build/test $i $2 $3 $4 $5 $6 $7 & 
+nohup ./build/autotest $i $2 $3 $4 $5 $6 $7 & 
 done
